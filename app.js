d2a2c30f2d41db662f44ffe1b75137c4d7e80989
Daniel Vallejo <danielsantiagovallejoo98@gmail.com>
2021-07-12 14:42:06 -0500

    sin passport

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const app = express();
-
+const bcrypt = require('bcrypt');
 const encrypt = require("mongoose-encryption")
-
+const saltRounds = 10;
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
@@ -38,17 +38,20 @@ app.get("/register",(req,res)=>{
 });
 
 app.post("/register",(req,res)=>{
-  const newUser =new User({
-    email:req.body.username,
-    password: req.body.password
-  })
-  newUser.save((err)=>{
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets")
-    }
-  })
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser =new User({
+      email:req.body.username,
+      password: hash
+    })
+    newUser.save((err)=>{
+      if(err){
+        console.log(err);
+      }else{
+        res.render("secrets")
+      }
+    })
+});
+
 });
 
 app.post("/login",(req,res)=>{
@@ -58,8 +61,14 @@ app.post("/login",(req,res)=>{
     if(err){
       console.log(err);
     }else{
-      if (foundUser.password===password) {
-        res.render("secrets")
+
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+        if (result) {
+          res.render("secrets");
+        }
+        });
+
       }
     }
   })
